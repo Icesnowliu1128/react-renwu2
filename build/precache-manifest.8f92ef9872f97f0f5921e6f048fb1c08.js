3da1eea6843de2e55145d7a1d6f96c057158a685
Icesnowliu1128 <3194529680@qq.com>
2020-10-18 11:16:21 +0800

    xueli

---
+++ build/precache-manifest.8f92ef9872f97f0f5921e6f048fb1c08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faa34f8f097b2d4afbbf18f076e1861",
+    "url": "/index.html"
+  },
+  {
+    "revision": "012e38ce3683d8f58e00",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "160d012c6d243ea2b053",
+    "url": "/static/css/main.3d911e95.chunk.css"
+  },
+  {
+    "revision": "012e38ce3683d8f58e00",
+    "url": "/static/js/2.d6e8aa08.chunk.js"
+  },
+  {
+    "revision": "de5fa9cfc3624bea3d7565f048bfaaaf",
+    "url": "/static/js/2.d6e8aa08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160d012c6d243ea2b053",
+    "url": "/static/js/main.f0046cad.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
